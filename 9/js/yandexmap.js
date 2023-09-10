2cf8364f89bdba16f34e55c80676b8ce5d28aec8
Keks <keks@htmlacademy.ru>
2023-09-10 17:37:12 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/yandexmap.js
@@ -0,0 +1,1 @@
+let map;function initMap(){map=new ymaps.Map("yandexmap",{center:[59.938631,30.323037],zoom:14}),myPlacemark=new ymaps.Placemark([59.938631,30.323037],{hintContent:"Расположение HTML Academy"},{iconLayout:"default#image",iconImageHref:"../img/map/pin.png",iconImageSize:[56,52],iconImageOffset:[-5,-38]}),map.geoObjects.add(myPlacemark)}ymaps.ready(initMap);
