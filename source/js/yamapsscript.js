828180bdab163ca1816c9c697bb6ffcd19a26867
egorReka <e.reka55@yandex.ru>
2023-09-12 20:40:12 +0300

    add api yamap

---
+++ source/js/yamapsscript.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const createScript = function (link) {
+    const body = document.body;
+    const element = document.createElement('script');
+    element.setAttribute('src', link);
+    body.appendChild(element);
+    return element;
+  }
+
+  const onScroll = function () {
+    const scrollTop = window.scrollY;
+
+    if (scrollTop > 100) {
+      const script = createScript('https://api-maps.yandex.ru/2.1/?apikey=31646f34-5cbb-4f95-bfd8-49550feff3a8&lang=ru_RU');
+      script.addEventListener('load', function () {
+        createScript('js/yandexmap.js');
+      });
+      document.removeEventListener('scroll', onScroll);
+    }
+  }
+
+  document.addEventListener('scroll', onScroll);
+});
--- source/js/yandexmap.js
+++ source/js/yandexmap.js
@@ -4,34 +4,19 @@ function initMap() {
 
   map = new ymaps.Map("yandexmap", {
     center: [59.938631, 30.323037],
-    zoom: 14
+    zoom: 14,
+    autofitToViewport: 'always'
   });
 
   myPlacemark = new ymaps.Placemark([59.938631, 30.323037], {
     hintContent: 'Расположение HTML Academy'
   }, {
-    // Опции.
-    // Необходимо указать данный тип макета.
     iconLayout: 'default#image',
-    // Своё изображение иконки метки.
     iconImageHref: '../img/map/pin.png',
-    // Размеры метки.
     iconImageSize: [56, 52],
-    // Смещение левого верхнего угла иконки относительно
-    // её "ножки" (точки привязки).
     iconImageOffset: [-5, -38]
   });
   map.geoObjects.add(myPlacemark);
 }
+
 ymaps.ready(initMap);
-
-
-// scroll
-
-// let scrollHeight = Math.max(
-//   document.body.scrollHeight, document.documentElement.scrollHeight,
-//   document.body.offsetHeight, document.documentElement.offsetHeight,
-//   document.body.clientHeight, document.documentElement.clientHeight
-// );
-
-// console.log('Полная высота документа с прокручиваемой частью: ' + scrollHeight);
